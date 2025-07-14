b52cd97a5fe6897d2c417cb75fe716818fe8aa2d
hainguyen011 <hainguyen0112358@gmail.com>
2025-07-15 05:48:59 +0700

    Test: Demo test Chat feature with chatbot using API from server

---
+++ src/apis/agentApis.jsx
@@ -0,0 +1,34 @@
+// lib/api/agentApi.ts
+
+
+export async function callAgentApi(payload) {
+  console.log(payload)
+  try {
+    const res = await fetch('https://fired-entire-excited-compatible.trycloudflare.com/api/agent', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        text: payload,
+      }),
+    });
+
+    if (!res.ok) {
+      throw new Error(`Server responded with status ${res.status}`);
+    }
+    const response = await res.json();
+    return {
+      success: response.success,
+      data: response.data,
+      message: response.message
+    };
+  } catch (error) {
+    console.error('Lỗi khi gọi API /api/agent:', error);
+    return {
+      success: false,
+      data: null,
+      message: "Lỗi khi gọi Agent"
+    };
+  }
+}
--- src/chatbot/Chatbot.jsx
+++ src/chatbot/Chatbot.jsx
@@ -1,10 +1,63 @@
-import { useState } from "react"
+'use client'
+
+import React, { useEffect, useRef, useState } from "react"
 import "./Chatbot.scss"
+import { callAgentApi } from "../apis/agentApis";
+import clsx from 'clsx';
+
+
 export const Chatbot = () => {
     const [isFormActive, setIsFormActive] = useState(false)
+    const [input, setInput] = useState("")
+    const [messages, setMessages] = useState([]);
+    const bottomRef = useRef(null);
+
+    
+    useEffect(() => {
+        bottomRef.current?.scrollIntoView({ behavior: "smooth" });
+    }, [messages]);
+
     const onActiveForm = () => {
         setIsFormActive(!isFormActive)
     }
+    const addUserChat = (input) => {
+        setMessages((prevMessages) => {
+            const isBotReplLastMsg = prevMessages.length > 0
+                ? prevMessages[prevMessages.length - 1].role === "bot"
+                : true;
+
+            if (!isBotReplLastMsg) {
+                // Nếu người dùng tiếp tục nói, thêm vào tin nhắn trước
+                const updated = [...prevMessages];
+                updated[updated.length - 1].content.push(input);
+                return updated;
+            } else {
+                // Nếu tin nhắn trước là bot hoặc chưa có tin nhắn, tạo mới
+                return [...prevMessages, { role: "user", content: [input] }];
+            }
+        });
+
+    }
+    const addChatbotReplied = (output) => {
+        setMessages((prevMessages) => [
+            ...prevMessages,
+            {
+                role: "bot",
+                content: [...output],
+            },
+        ]);
+    }
+    const onChat = () => {
+        addUserChat(input)
+
+        callAgentApi(input).then(result => {
+            const replied = result.data.agent_response.content
+            addChatbotReplied(replied)
+        })
+
+        setInput("")
+    }
+
     return (
         <div className="Chatbot">
             <div className={"chatbot-form " + (isFormActive ? "active" : "")}>
@@ -24,37 +77,57 @@ export const Chatbot = () => {
                 <div className="chatbot-body">
                     <div className="chatbot-body_msg-list">
                         {/* Render Message */}
-                        <div className="chatbot-body_card-message msg-reply">
-                            <div className="avatar">
-                                <img src="chatbot.jpg" />
-                            </div>
-                            <div className="content">
-                                <p className="msg-item">Chào bạn mình là Anton, mình có thể giúp bạn tra cứu đơn hàng hoặc thông tin về sản phẩm. </p>
-                                <p className="msg-item">Đừng ngại ngần chia sẻ cho mình biết nhé !</p>
-                            </div>
-                        </div>
-                        <div className="chatbot-body_card-message msg-send">
-                            <div className="content">
-                                <p className="msg-item">DCM bạn !</p>
-                            </div>
-                        </div>
+                        {
+                            messages.map(msg => {
+                                return <div
+                                    className={clsx('chatbot-body_card-message', {
+                                        'msg-reply': (msg.role == "bot"),
+                                        'msg-send': (msg.role == "user")
+                                    })}
+                                >
+                                    <div className="avatar">
+                                        <img src="chatbot.jpg" />
+                                    </div>
+                                    <div className="content">
+                                        {
+                                            msg.content.map((content, i) => {
+                                                return <p className="msg-item chat-animate"    style={{ animationDelay: `${i * 1.4}s` }}>{content}</p>
+                                            })
+                                        }
+                                    </div>
+                                </div>
+                            })
+                        }
                     </div>
+                    <div ref={bottomRef} /> 
                 </div>
 
                 <div className="chatbot-input">
-                    <div className="chatbot-input_suggests">
+                    {/* <div className="chatbot-input_suggests">
                         <p className="suggest-item">Tra cứu đơn hàng</p>
                         <p className="suggest-item">Tra sản phẩm</p>
                         <p className="suggest-item">Khiếu nại</p>
-                    </div>
+                    </div> */}
                     <div className="chatbot-input_area">
-                        <input type="text" id="chat-input" placeholder="Enter Messages" />
-                        <button id="send-button"><i class="bi bi-send"></i></button>
+                        <input
+                            type="text"
+                            id="chat-input"
+                            placeholder="Enter Messages"
+                            value={input}
+                            onChange={(e) => setInput(e.target.value)}
+                            onKeyDown={(e) => {
+                                if (e.key === 'Enter') {
+                                    e.preventDefault();
+                                    onChat();
+                                }
+                            }}
+                        />
+                        <button id="send-button" onClick={onChat}><i class="bi bi-send"></i></button>
                     </div>
                     <div className="chatbot-input_actions">
                         <button className="emoji-button" ><i class="bi bi-emoji-smile-fill"></i></button>
                         <label class="attachment-button">
-                            <input type="file" onchange="handleAttachment(this)" hidden />
+                            <input type="file" hidden />
                             <i class="bi bi-paperclip"></i>
                         </label>
                     </div>
